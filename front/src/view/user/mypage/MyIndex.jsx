5ee1526597e34478d74064bc53ee2e840c34c7fe
Gunmo Lee <rjsah5676@gmail.com>
2025-04-01 12:01:53 +0900

    마이페이지

--- front/src/view/user/mypage/MyIndex.jsx
+++ front/src/view/user/mypage/MyIndex.jsx
@@ -8,19 +8,49 @@ import '../../../css/view/mypage.css';
 function MyIndex(){
     const location = useLocation();
     const [path, setPath] = useState({f_name:'',l_name:''});
-    useEffect(()=> {
-        let pathname = location.pathname.split('/');
-        if(pathname[2] == 'profile') setPath({f_name:'내 정보', l_name:'프로필'});
-        else if(pathname[2] == 'edit') setPath({f_name:'내 정보', l_name:'개인 정보 수정'});
-        else if(pathname[2] == 'posts') setPath({f_name:'나의 활동', l_name:'작성한 글'});
-        else if(pathname[2] == 'wish') setPath({f_name:'보관함', l_name:'장바구니'});        
-        else setPath({f_name:pathname[pathname.length-2], l_name:pathname[pathname.length-1]});
-    },[location])
+    
+    useEffect(() => {
+        let pathname = location.pathname.split("/");
+        let page = pathname[2];
+
+        const pathMap = {
+            profile: { f_name: "내 정보", l_name: "프로필" },
+            edit: { f_name: "내 정보", l_name: "개인 정보 수정" },
+            posts: { f_name: "나의 활동", l_name: "작성한 글" },
+            reviews: { f_name: "나의 활동", l_name: "리뷰 관리" },
+            comments: { f_name: "나의 활동", l_name: "댓글 관리" },
+            reports: { f_name: "나의 활동", l_name: "신고 내역" },
+            inquiries: { f_name: "나의 활동", l_name: "문의 내역" },
+            purchases: { f_name: "거래 내역", l_name: "구매 기록" },
+            sales: { f_name: "거래 내역", l_name: "판매 기록" },
+            wish: { f_name: "보관함", l_name: "장바구니" },
+            coupons: { f_name: "보관함", l_name: "쿠폰함" },
+            points: { f_name: "보관함", l_name: "적립 내역" },
+            "stats-activity": { f_name: "통계", l_name: "활동 통계" },
+            "stats-purchases": { f_name: "통계", l_name: "구매 통계" },
+            "stats-sales": { f_name: "통계", l_name: "판매 통계" },
+            "delete-account": { f_name: "회원 탈퇴", l_name: "회원 탈퇴" },
+        };
+
+        if (pathMap[page]) {
+            setPath(pathMap[page]);
+        } else if (pathname.length > 3) {
+            setPath({ f_name: pathname[pathname.length - 2], l_name: pathname[pathname.length - 1] });
+        } else {
+            setPath({ f_name: "마이페이지", l_name: "" });
+        }
+    }, [location]);
+
 
     return(<>
         <div className='mypage-container'>
-            <MyPageHeader path={path} setPath={setPath}/>
-            <MyPageNav path={path} setPath={setPath}/>
+        </div>
+        <MyPageHeader path={path} setPath={setPath}/>
+        <MyPageNav path={path} setPath={setPath}/>
+        <div className='mypage-wrap'>
+            <div className='mypage-box'>
+                {location.pathname}
+            </div>
         </div>
     </>)
 }
--- front/src/view/user/mypage/MyPageNav.jsx
+++ front/src/view/user/mypage/MyPageNav.jsx
@@ -1,45 +1,80 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function MyPageNav({path,setPath}){
-    return(
-        <div className='mypage-nav'>
+function MyPageNav({ path }) {
+    const clickedStyle = { fontWeight: "bold", backgroundColor: "#555" };
+    const clickedStyle2 = { fontWeight: "bold", color: "#fff" };
+    const navigate = useNavigate();
+
+    return (
+        <div className="mypage-nav">
             <ul>
                 <li>내 정보</li>
-                <li><Link to="/mypage/profile">프로필</Link></li>
-                <li><Link to="/mypage/edit">개인 정보 수정</Link></li>
+                <li onClick={() => navigate("/mypage/profile")} style={path.l_name === "프로필" ? clickedStyle : {}}>
+                    <span style={path.l_name === "프로필" ? clickedStyle2 : {}}>프로필</span>
+                </li>
+                <li onClick={() => navigate("/mypage/edit")} style={path.l_name === "개인 정보 수정" ? clickedStyle : {}}>
+                    <span style={path.l_name === "개인 정보 수정" ? clickedStyle2 : {}}>개인 정보 수정</span>
+                </li>
             </ul>
             <ul>
                 <li>나의 활동</li>
-                <li><Link to="/mypage/posts">작성한 글</Link></li>
-                <li><Link to="/mypage/reviews">리뷰 관리</Link></li>
-                <li><Link to="/mypage/comments">댓글 관리</Link></li>
-                <li><Link to="/mypage/reports">신고 내역</Link></li>
-                <li><Link to="/mypage/inquiries">문의 내역</Link></li>
+                <li onClick={() => navigate("/mypage/posts")} style={path.l_name === "작성한 글" ? clickedStyle : {}}>
+                    <span style={path.l_name === "작성한 글" ? clickedStyle2 : {}}>작성한 글</span>
+                </li>
+                <li onClick={() => navigate("/mypage/reviews")} style={path.l_name === "리뷰 관리" ? clickedStyle : {}}>
+                    <span style={path.l_name === "리뷰 관리" ? clickedStyle2 : {}}>리뷰 관리</span>
+                </li>
+                <li onClick={() => navigate("/mypage/comments")} style={path.l_name === "댓글 관리" ? clickedStyle : {}}>
+                    <span style={path.l_name === "댓글 관리" ? clickedStyle2 : {}}>댓글 관리</span>
+                </li>
+                <li onClick={() => navigate("/mypage/reports")} style={path.l_name === "신고 내역" ? clickedStyle : {}}>
+                    <span style={path.l_name === "신고 내역" ? clickedStyle2 : {}}>신고 내역</span>
+                </li>
+                <li onClick={() => navigate("/mypage/inquiries")} style={path.l_name === "문의 내역" ? clickedStyle : {}}>
+                    <span style={path.l_name === "문의 내역" ? clickedStyle2 : {}}>문의 내역</span>
+                </li>
             </ul>
             <ul>
                 <li>거래 내역</li>
-                <li><Link to="/mypage/purchases">구매 기록</Link></li>
-                <li><Link to="/mypage/sales">판매 기록</Link></li>
+                <li onClick={() => navigate("/mypage/purchases")} style={path.l_name === "구매 기록" ? clickedStyle : {}}>
+                    <span style={path.l_name === "구매 기록" ? clickedStyle2 : {}}>구매 기록</span>
+                </li>
+                <li onClick={() => navigate("/mypage/sales")} style={path.l_name === "판매 기록" ? clickedStyle : {}}>
+                    <span style={path.l_name === "판매 기록" ? clickedStyle2 : {}}>판매 기록</span>
+                </li>
             </ul>
             <ul>
                 <li>보관함</li>
-                <li><Link to="/mypage/wish">장바구니</Link></li>
-                <li><Link to="/mypage/coupons">쿠폰함</Link></li>
-                <li><Link to="/mypage/points">적립 내역</Link></li>
+                <li onClick={() => navigate("/mypage/wish")} style={path.l_name === "장바구니" ? clickedStyle : {}}>
+                    <span style={path.l_name === "장바구니" ? clickedStyle2 : {}}>장바구니</span>
+                </li>
+                <li onClick={() => navigate("/mypage/coupons")} style={path.l_name === "쿠폰함" ? clickedStyle : {}}>
+                    <span style={path.l_name === "쿠폰함" ? clickedStyle2 : {}}>쿠폰함</span>
+                </li>
+                <li onClick={() => navigate("/mypage/points")} style={path.l_name === "적립 내역" ? clickedStyle : {}}>
+                    <span style={path.l_name === "적립 내역" ? clickedStyle2 : {}}>적립 내역</span>
+                </li>
             </ul>
             <ul>
                 <li>통계</li>
-                <li><Link to="/mypage/stats/activity">활동 통계</Link></li>
-                <li><Link to="/mypage/stats/purchases">구매 통계</Link></li>
-                <li><Link to="/mypage/stats/sales">판매 통계</Link></li>
+                <li onClick={() => navigate("/mypage/stats-activity")} style={path.l_name === "활동 통계" ? clickedStyle : {}}>
+                    <span style={path.l_name === "활동 통계" ? clickedStyle2 : {}}>활동 통계</span>
+                </li>
+                <li onClick={() => navigate("/mypage/stats-purchases")} style={path.l_name === "구매 통계" ? clickedStyle : {}}>
+                    <span style={path.l_name === "구매 통계" ? clickedStyle2 : {}}>구매 통계</span>
+                </li>
+                <li onClick={() => navigate("/mypage/stats-sales")} style={path.l_name === "판매 통계" ? clickedStyle : {}}>
+                    <span style={path.l_name === "판매 통계" ? clickedStyle2 : {}}>판매 통계</span>
+                </li>
             </ul>
             <ul>
-                <li>회원 탈퇴</li>
-                <li><Link to="/mypage/delete-account">회원 탈퇴</Link></li>
+                <li className='mypage-nav-title'>회원 탈퇴</li>
+                <li onClick={() => navigate("/mypage/delete-account")} style={path.l_name === "회원 탈퇴" ? clickedStyle : {}}>
+                    <span style={path.l_name === "회원 탈퇴" ? clickedStyle2 : {}}>회원 탈퇴</span>
+                </li>
             </ul>
-
         </div>
-    )
+    );
 }
 
 export default MyPageNav;
