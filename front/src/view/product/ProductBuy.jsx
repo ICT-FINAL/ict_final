6e6f9b2a588d03caf2c3e2aff75bd7913c273390
Gunmo Lee <rjsah5676@gmail.com>
2025-04-08 10:52:19 +0900

    상품 info 완

--- front/src/view/product/ProductBuy.jsx
+++ front/src/view/product/ProductBuy.jsx
@@ -3,8 +3,7 @@ import { useLocation } from "react-router-dom";
 
 function ProductBuy() {
   const location = useLocation();
-  const { productId, price, productName } = location.state || {};
-
+  const { productId, totalPrice, productName,selectedOptions } = location.state || {};
   const handlePayment = () => {
     if (!window.TossPayments) {
       alert("TossPayments SDK가 로드되지 않았습니다.");
@@ -14,10 +13,10 @@ function ProductBuy() {
       "test_ck_ORzdMaqN3w2RZ1XBgmxM85AkYXQG"
     );
     const orderId = "orderId-" + new Date().getTime();
-
+    console.log(selectedOptions);
     tossPayments
       .requestPayment("카드", {
-        amount: price,
+        amount: totalPrice,
         orderId,
         orderName: productName,
         customerName: "홍길동",
@@ -33,7 +32,7 @@ function ProductBuy() {
     <div style={{ padding: "100px" }}>
       <h2>상품 결제</h2>
       <p>상품명: {productName}</p>
-      <p>가격: {price}원</p>
+      <p>가격: {totalPrice}원</p>
       <button onClick={handlePayment}>💳 결제하기</button>
     </div>
   );
