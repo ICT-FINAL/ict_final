a5bdbff40396838b1f344657b76282fe50129c55
kangsora0102 <sr5230@hanmail.net>
2025-04-11 15:50:57 +0900

    상품에 대한 리뷰 기능 수정중
    상품에 대한 리뷰 기능 수정중

--- serv/src/main/java/com/ict/serv/repository/review/ReviewRepository.java
+++ serv/src/main/java/com/ict/serv/repository/review/ReviewRepository.java
@@ -1,8 +1,16 @@
 package com.ict.serv.repository.review;
 
+import com.ict.serv.entity.product.Product;
 import com.ict.serv.entity.review.Review;
+import com.ict.serv.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    
+
+    Optional<Review> findByUserAndProduct(User user, Product product);
+
+    List<Review> findByProduct(Product product);
 }
