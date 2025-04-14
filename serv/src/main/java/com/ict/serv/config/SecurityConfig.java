aca15081833ea0f09cfd71afebf93f691208e2f4
Gunmo Lee <rjsah5676@gmail.com>
2025-04-14 10:46:18 +0900

    삭제 공용

--- serv/src/main/java/com/ict/serv/config/SecurityConfig.java
+++ serv/src/main/java/com/ict/serv/config/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                                 "/auth/reset-password/verify",
                                 "/auth/reset-password",
                                 "/auth/me",
-                                "/auction/room/**"
+                                "/auction/room/**",
+                                "/auction/**"
                         ).permitAll()
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/signup/**", "/auth/login").permitAll()
@@ -70,7 +71,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000"); // 배포 시 서버 주소로
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("http://192.168.1.146:3000"); // 배포 시 서버 주소로
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         configuration.addAllowedHeader("*"); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 인증 정보 포함 허용
