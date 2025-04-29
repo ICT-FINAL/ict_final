0110f96cae9e1e36027da2e5baf88af4e1535394
kangsora0102 <sr5230@hanmail.net>
2025-04-29 18:22:22 +0900

    기능 수정 및 디자인 수정
    리뷰 파일 용량 10메가로 제한, 서브메뉴 삭제시 정말 삭제하시겠습니까? 문구 추가, 이벤트 수정, 삭제 기능 추가, 메인 페이지 등등... 디자인 여기저기 수정....!

--- front/src/view/event/EventInfo.jsx
+++ front/src/view/event/EventInfo.jsx
@@ -1,13 +1,31 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { setModal } from "../../store/modalSlice";
+import axios from "axios";
 
 function EventInfo() {
+    const modal = useSelector((state)=>state.modal);
+
     const loc = useLocation();
     const { eventName, filename, startDate, endDate, content, id } = loc.state;
 
     const serverIP = useSelector((state) => state.serverIP);
+    const user = useSelector((state) => state.auth.user);
 
     const navigate = useNavigate();
+
+    const [searchParams] = useSearchParams();
+    const [activeTab, setActiveTab] = useState("ongoing");
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const tab = searchParams.get("activeTab"); // URL에서 activeTab 꺼냄
+        if (tab) {
+            setActiveTab(tab); // 있으면 set 해주기
+        }
+    }, [searchParams]);
+
 
     const handleEditClick = () => {
         if (id) {
@@ -16,6 +34,26 @@ function EventInfo() {
             alert("이벤트 ID가 없습니다.");
         }
     };
+
+    const handleNoEditClick = () => {
+        alert("종료된 이벤트는 수정할 수 없습니다.");
+    }
+
+    useEffect(()=>{
+        if(modal.delCheck==='event') {
+            axios.get(`${serverIP.ip}/event/delEvent?eventId=${modal.selected.split('-')[2]}`,{
+                headers: { Authorization: `Bearer ${user.token}` } 
+            })
+            .then(res=>{
+                if(res.data==="delEventOk"){
+                    dispatch(setModal({delCheck:''}));
+                    navigate(`/event`);
+                }
+            })
+            .catch(err => console.log(err));
+        }
+    },[modal.delCheck])
+
 
     return (
         <div style={{ paddingTop: "150px" }}>
@@ -31,10 +69,13 @@ function EventInfo() {
                     className="event-info-content"
                     dangerouslySetInnerHTML={{ __html: content }}
                 ></div>
-                <div style={{textAlign:'right'}}>
-                    <input type='button' value='수정' className="edit-button" style={{marginRight:'5px'}} onClick={handleEditClick}/>
-                    <input type='button' value='삭제' className="del-button" />
-                </div>
+                {user && user.user.authority === "ROLE_ADMIN" && (
+                    <div style={{textAlign:'right'}}>
+                        {activeTab !== "ended" && <input type='button' value='수정' className="edit-button" style={{marginRight:'5px'}} onClick={handleEditClick}/>}
+                        {activeTab === "ended" && <input type='button' value='수정' className="edit-button" style={{marginRight:'5px'}} onClick={handleNoEditClick}/>}
+                        <input type='button' value='삭제' className="del-button" id={`event-delll-${id}`}/>
+                    </div>
+                )}
             </div>
         </div>
     );
